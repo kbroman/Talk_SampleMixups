988f9ce534aed094d061160312fc9235118bc292
Karl Broman <kbroman@gmail.com>
2017-04-03 21:40:12 -0500

    data_fig: black background, phenotype as image

--- R/data_fig.R
+++ R/data_fig.R
@@ -1,18 +1,31 @@
 # image of genotypes + phenotypes
 
+library(broman)
+library(qtl)
+
 png("../Figs/data_fig.png", height=1000, width=1500, pointsize=28)
+
+bgcolor <- broman::brocolors("bgpng")
+par(bg=bgcolor, col.main="white", col="white",
+    fg="white", col.lab="white", col.axis="white")
 
 load("~/Projects/Attie/GoldStandard/FinalData/aligned_geno_with_pmap.RData")
 load("~/Projects/Attie/GoldStandard/FinalData/lipomics_final_rev2.RData")
 phe <- lipomics[,c("MouseNum","INSULIN (ng/ml) 10 wk"),drop=FALSE]
 
+# reduce markers
+reduced <- NULL
+for(i in 1:nchr(f2g))
+    reduced <- c(reduced, pickMarkerSubset(f2g$geno[[i]]$map, 1))
+f2g <- pull.markers(f2g, reduced)
+
 id <- lineup::findCommonID(f2g$pheno$MouseNum, phe$MouseNum)
 f2g <- subset(f2g, ind=id$first)
 f2g$pheno <- cbind(f2g$pheno, insulin=phe[id$second,2])
 
-layout(cbind(1,2), width=c(1,0.22))
+layout(cbind(1,2), width=c(1, 0.12))
 
-color <- broman::brocolors("crayons")[c("White", "Cornflower", "Dandelion", "Blush")]
+color <- c(bgcolor, broman::brocolors("crayons")[c("Cornflower", "Dandelion", "Blush")])
 par(mar=c(5.1,4.1,3.6,0.6))
 names(f2g$geno)[20] <- ""
 geno.image(f2g, col=color, main="", ylab="Mice", reorder=nphe(f2g))
@@ -20,9 +33,15 @@ u <- par("usr")
 axis(side=3, at=u[2], labels="X", xpd=TRUE, line=-0.5)
 
 y <- log10( f2g$pheno$insulin[order(f2g$pheno$insulin)] )
-par(cex.lab=0.8, cex.axis=0.75)
-grayplot(y, seq_along(y), xlab="log10 insulin", ylab="Mice", bgcolor="gray90",
-         yaxs="i", ylim=c(0.5, length(y)+0.5), pch=16, col="slateblue",
-         xaxs="i", cex=0.8, xlim=range(y)*c(0.98, 1.02))
+par(mar=c(5.1,1.8,3.6,1.8))
+
+ramp <- c(colorRampPalette(c(broman::brocolors("crayons")["Cornflower"], "white"))(128),
+          colorRampPalette(c("white",broman::brocolors("crayons")["Blush"]))(128)[-1])
+
+image(1, seq(along=y), rbind(y), col=ramp,
+      xlab="", xaxt="n", ylab="", yaxt="n")
+axis(side=2, at=seq(100, 500, by=100), label=rep("", 5))
+axis(side=3, at=mean(par("usr")[1:2]), label="phenotype",
+     line=-0.5, xpd=TRUE, tick=FALSE)
 
 dev.off()
